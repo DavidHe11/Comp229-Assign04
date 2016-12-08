b2e929bdc99e37de8c4085a3b2eb41fd75a1d194
DavidHe11 <davidyhe1194@gmail.com>
2016-12-08 13:53:28 -0500

    Added Json file, Created model class, minor master site edits, Global created modelcreation method and jsonfilecreation method.

--- Comp229-Assign04/Global.asax.cs
+++ Comp229-Assign04/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
@@ -11,11 +13,34 @@ namespace Comp229_Assign04
 {
     public class Global : HttpApplication
     {
+        public static IEnumerable<ModelData> Models;
+        private static string jsonModelFilePath = ("~/Data/Assign04.json");
+        private static string jsonOutputFilePath = ("~/Data/NewModelsAssign04.json");
+
+
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-    }
-}
+
+        private void createModelCollection()
+        {
+            /*Creates a IEnumberable of Models using the modelData*/
+            using (StreamReader reader = new StreamReader(System.Web.Hosting.HostingEnvironment.MapPath(jsonModelFilePath)))
+            {
+                var jsonString = reader.ReadToEnd();
+                Models = JsonConvert.DeserializeObject<List<ModelData>>(jsonString);
+            };
+            
+         }
+        public static void CreateAJsonFile()
+        {
+            /*Creates a Json file with new data.*/
+            using (StreamWriter writer = File.CreateText(System.Web.Hosting.HostingEnvironment.MapPath(jsonOutputFilePath)))
+            {
+                writer.WriteLine(JsonConvert.SerializeObject(Models));
+            }
+
+        }
